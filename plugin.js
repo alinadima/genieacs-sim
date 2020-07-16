dab8b1e3e4502ecf0b5a8e0e02c8c5d6e8faa3bb
Alina Dima <dimaalin@amazon.com>
2020-07-16 15:47:15 +0200

    Fix plugin

--- plugin.js
+++ plugin.js
@@ -5,30 +5,29 @@ class Plugin {
     }
 
     onEventReceived(event) {
-        console.log("onEventReceived");
+        console.log("This function should implement the event handling operation specific to your plugin, based on TR69 changes.")
     }
 
 
     setUp(device) {
-        console.log("setUp");
+        console.log("This function should implement the setup operation specific to your plugin.")
     }
 
     perform() {
-        console.log("perform");
+        console.log("This function should implement the perform main operation specific to your plugin.")
     }
 
     handleSetParameters(name, device) {
-        console.log("Handle set parameters");
+        console.log("This function should implement your specific handling for your parameters set via TR69.")
     }
 
     performFresh() {
-        console.log("This function should implement the perform fresh operation specific to any plugin.")
+        console.log("This function should implement the perform fresh operation specific to your plugin.")
     }
 
     addPluginParamsToDataModel(exisitingParamsArray) {
-        console.log("Add Plugin params");
+        console.log("This function should add your plugin specific params to the TR69 data model of the device.")
     }
-
 }
 
 module.exports = Plugin;
